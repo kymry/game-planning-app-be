107a875f97211a7d7c86e8441cc98399fdfdca20
Justin Kim <kimjustin@team-lab.com>
2025-04-17 13:00:29 +0900

    Move files around to create structure and create a few routes

---
+++ src/api/routes/home.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+
+export default (app: Router) => {
+  const router = Router()
+  app.use("/", router)    
+
+  router.get('', (_req, res) => {
+    res.send({"home": "Hello from the bangtang boiiissssss"});
+  });
+      
+  return app;    
+}
